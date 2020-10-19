a802ba2c59128acd2e93b7edad3305aa0ac14ab6
m23293273 <matinhuseynzade@std.iyte.edu.tr>
2020-10-19 09:31:47 +0000

    temperature

--- lab1/example1.py
+++ lab1/example1.py
@@ -1,4 +1,4 @@
-celcius = input("Input celcius value: )
+celcius = float(input("Input celcius value: "))
 
 fahrenheit = celcius * 1.8 +32
 print(fahrenheit)
--- lab2/Temperature.py
+++
@@ -1,4 +0,0 @@
-celcius = float(input("Input celcius value: "))
-
-fahrenheit = celcius * 1.8 +32
-print(fahrenheit)
