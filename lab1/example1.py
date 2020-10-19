c38f44a6d5a0802abd0ee099e87dd07b0e702bba
m23293273 <matinhuseynzade@std.iyte.edu.tr>
2020-10-19 09:05:50 +0000

    Equation

--- lab1/example1.py
+++ lab1/example1.py
@@ -1,4 +1,5 @@
-animals = 10
-chickens = 6
-roosters = animals - chickens
-print(roosters)
+x = 1
+y = 4
+z = 0.25
+print((((2*x+y)**2)*z**0.5)/((x**0.5)+(y**0.5)))
+(((2*x+y)**2)*(z**0.5))/((x**0.5)+(y**0.5))
