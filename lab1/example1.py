77846634debc3db474739fb4464e6da03b403af0
m23293273 <matinhuseynzade@std.iyte.edu.tr>
2020-10-23 21:56:30 +0000

    hello world

--- lab1/example1.py
+++ lab1/example1.py
@@ -1,10 +1,1 @@
-speed1 = 80 / 60  #kilometers per second
-speed2 = 70 / 60  #kilometers per second
-
-distance = 490-150  #total distance of movement
-sum_speed = speed1 + speed2 
-
-answer = distance / sum_speed
-
-print(answer)
-
+print("hello world")
