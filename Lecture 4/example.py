946f9deb97fc04ed949a4c9f075b7de0169243ce
chillmetin <matinhuseynzade@std.iyte.edu.tr>
2020-11-15 01:23:24 +0400

    lecture 4  max

--- Lecture 4/example.py
+++ Lecture 4/example.py
@@ -1,3 +1,12 @@
 inputs = input("Please, input values separated by commas: ")
-x1, x2, x3 = inputs.split(",")
-print(inputs)
+x1,x2,x3 = inputs.split(",")
+x1,x2,x3 = int(x1),int(x2),int(x3)
+
+if x1 >= x2 >= x3:
+    maximum = x1
+elif x2 >= x3:
+    maximum = x2
+else:
+    maximum = x3
+
+print("The largest value is", maximum)
