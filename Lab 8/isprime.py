4fdb342e61041d516ad9303dc5549c1ddea6fed2
m23293273 <matinhuseynzade@std.iyte.edu.tr>
2020-12-14 09:21:21 +0000

    prime

--- Lab 8/isprime.py
+++ Lab 8/isprime.py
@@ -2,8 +2,8 @@ def is_prime(number):
     prime = True
     for i in range(2, number//2+1):
         if number % i == 0:
-            prime == False
+            prime = False
     
     return prime
 
-    
+print(is_prime(11))
